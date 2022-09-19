284c8a33497a1ae3119e4dd116d1ffbcc82c266c
Siyu(Jack) You <61994603+Fantastisity@users.noreply.github.com>
2022-09-20 00:54:01 +1000

    Update explorePg.js

--- src/components/landmark/explorePg.js
+++ src/components/landmark/explorePg.js
@@ -1,40 +1,14 @@
 import React from "react";
 import "./explorePg.css";
-import { useNavigate } from "react-router-dom";
 import landmarkPic from "../../images/landmark.jpg";
 import musicPic from "../../images/music.jpg";
 import outdoorPic from "../../images/outdoor.jpg";
+import { Link } from "react-router-dom";
 
 const ExplorePg = () => {
-  let navigate = useNavigate();
-  const routeChange = () => {
-    let path = `./outdoor`;
-    navigate(path);
-  };
-
-  const exploreContents = [
-    {
-      pic: landmarkPic,
-      title: "Landmarks",
-      content:
-        "Discover Melbourne's best attractions and landmarks including beautiful gardens, impressive buildings, sporting arenas, markets and famous laneways.",
-    },
-    {
-      pic: musicPic,
-      title: "Live music",
-      content:
-        "Melbourne is widely known as one of the world's great music cities. Discover dedicated live music venues in Melbourne.",
-    },
-    {
-      pic: outdoorPic,
-      title: "Outdoor Artworks",
-      content:
-        "Discover Melbourne's outdoor artwork such as memorials and sculptures located around the city.",
-    },
-  ];
 
   return (
-    <div style={{ backgroundColor: "#FFF4E2", height: "2580px" }}>
+    <div style={{ backgroundColor: "#FFF4E2", height: "1000px" }}>
       <div className="headerBg">Explore Melbourne CBD</div>
       <div
         style={{
@@ -50,20 +24,19 @@ const ExplorePg = () => {
         Wondering where to explore more in Melbourne CBD? Here are some options
         for you to better experience the local culture.
       </div>
-      <div style={{ display: "flex", cursor: "pointer" }} onClick={routeChange}>
-        {exploreContents.map((ecs, index) => (
+      <div style={{ display: "flex", cursor: "pointer", marginLeft: "30px" }}>
+          <Link to='/landmark-map' style={{ textDecoration: 'none' }}>
           <div
             className="content-item"
-            key={index}
             style={{
               marginLeft: "4rem",
               marginTop: "3rem",
               marginRight: "2rem",
-              width: "30rem",
+              width: "22rem",
             }}
           >
             <img
-              src={ecs.pic}
+              src={landmarkPic}
               style={{
                 width: "22rem",
                 height: "15rem",
@@ -72,12 +45,66 @@ const ExplorePg = () => {
                 marginRight: "auto",
               }}
             />
-            <div style={{ textAlign: "center", width: "22rem" }}>
-              <h3>{ecs.title}</h3>
-              <p>{ecs.content}</p>
+            <div style={{ textAlign: "center", width: "22rem"}}>
+              <h3>Landmarks</h3>
+              <p>Discover Melbourne's best attractions and landmarks including beautiful gardens, impressive buildings, sporting arenas, markets and famous laneways.</p>
             </div>
           </div>
-        ))}
+          </Link>
+
+          <Link to='/music-map' style={{ textDecoration: 'none' }}>
+          <div
+            className="content-item"
+            style={{
+              marginLeft: "4rem",
+              marginTop: "3rem",
+              marginRight: "2rem",
+              width: "22rem",
+            }}
+          >
+            <img
+              src={musicPic}
+              style={{
+                width: "22rem",
+                height: "15rem",
+                display: "block",
+                marginLeft: "auto",
+                marginRight: "auto",
+              }}
+            />
+            <div style={{ textAlign: "center", width: "22rem"}}>
+              <h3>Live music</h3>
+              <p>Melbourne is widely known as one of the world's great music cities. Discover dedicated live music venues in Melbourne.</p>
+            </div>
+          </div>
+          </Link>
+
+          <Link to='/artwork-map' style={{ textDecoration: 'none' }}>
+            <div
+              className="content-item"
+              style={{
+                marginLeft: "4rem",
+                marginTop: "3rem",
+                marginRight: "2rem",
+                width: "22rem",
+              }}
+            >
+              <img
+                src={outdoorPic}
+                style={{
+                  width: "22rem",
+                  height: "15rem",
+                  display: "block",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                }}
+              />
+              <div style={{ textAlign: "center", width: "22rem"}}>
+                <h3>Outdoor Artworks</h3>
+                <p>Discover Melbourne's outdoor artwork such as memorials and sculptures located around the city.</p>
+              </div>
+            </div>
+          </Link>
       </div>
     </div>
   );
