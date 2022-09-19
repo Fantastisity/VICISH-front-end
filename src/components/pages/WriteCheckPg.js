f13f85a1b902d1977a462a77d841c7be01e1fadc
Siyu(Jack) You <61994603+Fantastisity@users.noreply.github.com>
2022-09-19 10:38:36 +1000

    Add files via upload

---
+++ src/components/pages/WriteCheckPg.js
@@ -0,0 +1,32 @@
+import React from "react";
+import WritingHandler from "../writingAssessment/WritingHandler";
+import "./WriteCheck.css";
+
+const WriteCheck = () => {
+  return (
+    <div style={{backgroundColor: "#F0BA98", height: "1000px"}}>
+      <div className="headerBg">Grammar Checker</div>
+      <div className="slangIntro" style={{
+          marginLeft: "320px", 
+          marginTop: "30px", 
+          borderRadius: "60px",
+          padding: "25px",
+          width: "56%",
+          fontSize: "30px",
+          textAlign: "center"}}>
+           Wondering where to improve your grammar? This grammar check feature helps you to identify any mistake made in a paragraph. 
+          </div>
+          <div className="titleBg" style={{
+          marginLeft: "550px", 
+          marginTop: "30px", 
+          borderRadius: "20px",
+          padding: "20px",
+          width: "25%",
+          backgroundColor: "#FFF4E2"}}>Enter your paragraph in the text box below</div>
+      <div className="inputDiv">{<WritingHandler />}</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default WriteCheck;
