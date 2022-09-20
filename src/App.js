950e30ab644e9111bb8497b5d235506ede07de27
Siyu(Jack) You <61994603+Fantastisity@users.noreply.github.com>
2022-09-21 09:54:18 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import Home from './components/pages/homePg';
 import Life from './components/pages/lifePg';
 import Entertainment from './components/pages/entertainPg';
 import ExplorePg from './components/landmark/explorePg';
-import LandmarkMap from './components/landmark/LandmarkMap';
-import ArtworkMap from './components/landmark/ArtworkMap';
-import Quizzes from './components/pages/quizzesPg';
+import VICISHMap from './components/landmark/Map';
 import SlangQuiz from './components/pages/slangQuizPg';
 import EnglishQuiz from './components/pages/englishQuiz';
 import Bank from './components/pages/bankPg';
-// import Footer from "./components/general/Footer";
 import VocabPg from './components/pages/VocabPg';
 import WriteCheck from './components/pages/WriteCheckPg';
 import Diet from './components/pages/dietPg';
@@ -21,7 +18,6 @@ import { MenuPlanner } from './components/menuplan/menuplanHandler';
 import PTV from './components/pages/ptvPg';
 import ScrollToTop from "./components/general/scrollToTop.js"
 import Transport from './components/pages/tranportPg';
-import MusicMap from './components/landmark/MusicMap';
 function App() {
   return (
     <div className='App'>
@@ -33,9 +29,7 @@ function App() {
         <Route path='/life' element={<Life/>} />
         <Route path='/entertainments' element={<Entertainment/>} />
         <Route path='/explore-melb' element={<ExplorePg/>} />
-        <Route path='/landmark-map' element={<LandmarkMap/>} />
-        <Route path='/artwork-map' element={<ArtworkMap/>} />
-        <Route path='/music-map' element={<MusicMap/>} />
+        <Route path='/map' element={<VICISHMap/>} />
         <Route path='/slang' element={<SlangQuiz/>} />
         <Route path='/english-quiz' element={<EnglishQuiz/>} />
         <Route path='/catch-keyword' element={<VocabPg />}/>
@@ -48,9 +42,6 @@ function App() {
         <Route path='/transport' element={<Transport/>} />\
       </Routes>
     </Router>
-
-    {/* <div><Footer/></div> */}
-    
     </div>
   );
 }
