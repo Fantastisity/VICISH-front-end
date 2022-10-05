6bdeb4bbf20d27762ee8ee744246ee265493e05c
Siyu(Jack) You <61994603+Fantastisity@users.noreply.github.com>
2022-10-05 21:20:03 +1100

    Add files via upload

---
+++ src/components/pages/WriteCheckPg.js
@@ -0,0 +1,59 @@
+import React from "react";
+import WritingHandler from "../writingAssessment/WritingHandler";
+import "./WriteCheck.css";
+import GrammarPic from "../../images/grammarPic.png";
+import GrammarPic2 from "../../images/GrammarPic2.png";
+import { fadeInDown } from 'react-animations';
+ import Radium, {StyleRoot} from 'radium';
+
+ const styles = {
+   fadeInDown: {
+     animation: 'x 2s',
+     animationName: Radium.keyframes(fadeInDown, 'fadeInDown'),
+   },
+  fadeInDown2: {
+     animation: 'x 4s',
+     animationName: Radium.keyframes(fadeInDown, 'fadeInDown'),
+   }
+ }
+
+const WriteCheck = () => {
+  return (
+    <div style={{backgroundColor: "#F0BA98", height: "100%"}}>
+      <div className="headerBg">Grammar Checker</div>
+<div style={{ display:"flex"}}>
+<StyleRoot>
+       <div style={styles.fadeInDown} className="grammarPic">
+       <img src={GrammarPic} width='500' height='1000'></img>
+       </div>
+       </StyleRoot>
+<StyleRoot>
+       <div style={styles.fadeInDown2} className="grammarPic2">
+       <img src={GrammarPic2} style={{marginLeft:"35%"}}></img>
+       </div>
+       </StyleRoot>
+       </div>
+      <div className="slangIntro" style={{
+          marginLeft: "320px", 
+          marginTop: "20%", 
+          borderRadius: "60px",
+          padding: "25px",
+          width: "56%",
+          fontSize: "30px",
+          textAlign: "center"}}>
+           Wondering where to improve your grammar? This grammar check feature helps you to identify any mistake made in a paragraph. 
+          </div>
+          <div className="titleBg" style={{
+          marginLeft: "35%", 
+          marginTop: "30px", 
+          borderRadius: "20px",
+          padding: "20px",
+          width: "25%",
+          backgroundColor: "#FFF4E2"}}>Enter your paragraph in the text box below</div>
+      <div className="inputDiv">{<WritingHandler />}</div>
+      <div></div>
+    </div>
+  );
+};
+
+export default WriteCheck;
